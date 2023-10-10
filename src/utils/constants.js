c2b68dc2b325d82d0e72915cf74967d8b109c796
Administrator <Administrator@172.31.36.201>
2023-10-10 10:27:47 +0000

    initial commit

---
+++ src/utils/constants.js
@@ -0,0 +1,7 @@
+export const BASE_URL = "http://3.133.64.67:8114/petcarev5be"; //Temporarily using field; Enter Backend URL here e.g. http://3.133.64.67:8164/generated_app
+export const PATH_PETSERVICE = "/PetServices";
+export const PATH_PETOWNER = "/PetOwners";
+export const PATH_PETCARECENTER = "/PetCareCenters";
+export const PATH_BUSINESSHOURS = "/BusinessHourses";
+export const PATH_DOCUMENT = "/Documents";
+export const PATH_PET = "/Pets";
---
+++ src/utils/display_configuration.js
@@ -0,0 +1,48 @@
+export const petCareCenterViewConfig = {
+Details: [
+    {
+      key: "PcId",
+      value: "PcId",
+      type: "text",
+      editable: false,
+      required: true
+    },
+    {
+      key: "Name",
+      value: "Name",
+      type: "text",
+      editable: false,
+      required: true
+    },
+    {
+      key: "BranchName",
+      value: "BranchName",
+      type: "text",
+      editable: false,
+      required: true
+    },
+    {
+      key: "Pincode",
+      value: "Pincode",
+      type: "text",
+      editable: false,
+      required: true
+    },
+    {
+      key: "Latitude",
+      value: "Latitude",
+      type: "text",
+      editable: false,
+      required: true
+    },
+    {
+      key: "Longitude",
+      value: "Longitude",
+      type: "text",
+      editable: false,
+      required: true
+    },
+  ],
+Type: [
+  ],
+}
